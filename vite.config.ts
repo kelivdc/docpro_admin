25f6a8a2ba042d227d567d77db3d6d445dedc1ba
Arie <kelivdc@yahoo.com>
2026-08-01 09:34:20 +0700

    Allowed Host

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,12 @@ import tailwindcss from '@tailwindcss/vite'
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  server: {
+    allowedHosts: ['admin.nexonace.com'],
+  },
+  preview: {
+    allowedHosts: ['admin.nexonace.com'],
+  },
 })
 
 export default config
